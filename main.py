eae285440b4bebed740738f00c17afece326f6a2
Inder-26 <inder26112004@zohomail.in>
2025-12-29 18:26:21 +0530

    Data Transformation done

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TraningPipelineConfig
 import sys
 
@@ -10,16 +12,23 @@ if __name__ == "__main__":
     try:
         traningpipelineconfig=TraningPipelineConfig()
         dataingestionconfig=DataIngestionConfig(traningpipelineconfig)
-        dataingestion=DataIngestion(dataingestionconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
-        dataingestionartifact=dataingestion.initiate_data_ingestion()
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info(f"Data ingestion completed {dataingestionartifact}")
         print(dataingestionartifact)
         data_validation_config=DataValidationConfig(traningpipelineconfig)
-        datavalidation=DataValidation(dataingestionartifact,data_validation_config)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiate the data validation")
-        data_validation_artifact=datavalidation.initiate_data_validation()
+        data_validation_artifact=data_validation.initiate_data_validation()
         logging.info(f"Data validation completed {data_validation_artifact}")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(traningpipelineconfig)
+        data_transformation=DataTransformation(data_transformation_config,data_validation_artifact)
+        logging.info("Initiate the data transformation")
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info(f"Data transformation completed {data_transformation_artifact}")
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
